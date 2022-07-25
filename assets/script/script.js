e4a93056c2594e9f852852aa9295d39752fe079f
fanylouise <gandra.fany@gmail.com>
2022-07-25 18:45:01 -0300

    clock implementation

---
+++ assets/script/script.js
@@ -0,0 +1,18 @@
+const currentTime = () =>{
+  const base = document.getElementById("clock");
+
+  let date = new Date();
+  let hh = date.getHours();
+  let mm = date.getMinutes();
+  let ss = date.getSeconds();
+
+  hh = hh < 10 ? `0${hh}` : hh;
+  mm = mm < 10 ? `0${mm}` : mm;
+  ss = ss < 10 ? `0${ss}` : ss;
+
+  let time = `${hh}:${mm}:${ss}`;
+  base.innerHTML = time;
+}
+
+currentTime();
+setInterval(currentTime, 1000);
