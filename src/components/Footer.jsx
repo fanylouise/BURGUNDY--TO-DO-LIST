640611638538c1ed69c78fecc30504c5c0cdc966
fanylouise <gandra.fany@gmail.com>
2022-08-31 12:42:15 -0300

    :pencil: changin word

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 const Footer = () => {
-  return ( <footer><p>Developer by <a href="https://github.com/fanylouise">FanyLouise</a></p></footer> );
+  return ( <footer><p>Developed by <a href="https://github.com/fanylouise">FanyLouise</a></p></footer> );
 }
  
 export default Footer;
